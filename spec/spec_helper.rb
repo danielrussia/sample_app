c4f89aed7cc71c8a3c538f2aaa818443c5c3f06d
Daniel <daniel@whereisdaniel.ru>
2010-09-24 13:02:23 +0400

    Chapter 09 ex 1 copmlete

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,6 +24,13 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
 
+  def integration_sign_in(user)
+    visit signin_path
+    fill_in :email,    :with => user.email
+    fill_in :password, :with => user.password
+    click_button
+  end
+
   def test_sign_in(user)
     controller.current_user = user
   end
